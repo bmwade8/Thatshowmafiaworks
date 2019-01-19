10b26fc995dd74a51c837add560287dbeb3d0a30
Ben Wade <benwade8@yahoo.com>
2019-01-18 17:35:17 -0800

    part 2 of tests

--- my_bot2.py
+++ my_bot2.py
@@ -1,8 +1,25 @@
+
+import random
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("?", "!")
-TOKEN = "NTM1MzI0NTI5NTUyMTMwMDUw.DyLG1w.EX4XPY4lcNErdtwF3bxtnmPQl_4"
+TOKEN = "NTM1MzI0NTI5NTUyMTMwMDUw.DyQKCQ.mPzAB06DcLNnMNmGAuM3_SrqhxE"
 
 client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.")
+async def eight_ball():
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely'
+    ]
+    await client.say(random.choice(possible_responses))
+
 
 client.run(TOKEN)
