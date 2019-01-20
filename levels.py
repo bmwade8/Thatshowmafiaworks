4e722c7358db1c8e421955f1a2a6f52f95531855
Ben Wade <bmwade8@gmail.com>
2019-01-19 18:21:15 -0800

    added russian roulette

--- levels.py
+++ levels.py
@@ -1,5 +1,6 @@
 
 import discord
+import random
 
 from discord.ext.commands import Bot
 
@@ -10,9 +11,28 @@ client = Bot(command_prefix=BOT_PREFIX)
 
 
 @client.event
-async def init_level(before, after):
+async def init_level(member):
+    before = member
     await discord.on_member_update(before, after)
-    client.change_nickname(after, "Lvl. 1 " + before.nick)
+    client.change_nickname(member, "Lvl. 1 " + before.nick)
+
+
+@client.command(pass_context=True,
+                name="russianroulette",
+                aliases=["rr", "russian_roulette", "russian"])
+async def russian_roulette(context):
+    possible_responses = [
+        " was just murdered to death...",
+        " has just been murdered! Reports claim that the victim knew the assailant",
+        " was their own worst enemy",
+        " flew too close to the sun"
+    ]
+    shot = random.randint(1, 6)
+    if shot == 6:
+        await client.say(context.message.author.mention +
+                         random.choice(possible_responses))
+    else:
+        await client.say(context.message.author.mention + " gained 3 levels.")
 
 
 client.run(TOKEN)
