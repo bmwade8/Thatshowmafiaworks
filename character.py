d9b3141e29b1edd82267378c2f3b24aa3a4182dd
Ben Wade <bmwade8@gmail.com>
2019-01-21 21:06:03 -0800

    changed nickname functionality to a command

--- character.py
+++ character.py
@@ -5,10 +5,13 @@ class Character:
     titles = ['Crook', 'Soldiers', 'Capo', 'Underboss', 'Mafia Boss',
               'Head Mafia Boss']
 
-    def __init__(self):
+    def __init__(self, member):
+        self.char_member = member
         self.level = 1
         self.meta_level = self.titles[0]
+        self.level_tag = 'Lvl 1 '
 
     def update_level(self, delta):
         self.level += delta
         self.meta_level = self.titles[int(self.level / 20)]
+        self.level_tag = 'Lvl ' + str(self.level) + ' '
--- levels.py
+++ levels.py
@@ -1,3 +1,5 @@
+from character import Character
+
 import discord
 import random
 
@@ -7,56 +9,63 @@ BOT_PREFIX = '+'
 TOKEN = 'NTM1MzI0NTI5NTUyMTMwMDUw.DyU5qw.bm9vNkgcLMl0L0EuSh0qzsgKwmY'
 
 client = Bot(command_prefix=BOT_PREFIX)
+toons = {}
 
 
+# TODO: UNTESTED
 @client.event
-async def on_member_update(before, after):
-    if after.nick == None:
-        new_nick = "Lvl 1 " + after.name
-    elif not after.nick.startswith("Lvl 1 "):
-        new_nick = "Lvl 1 " + after.nick
-    else:
-        new_nick = after.nick
-    # bot can never have permissions above server owner, so it can't change
-    # an owner's nickname
-    if before.server.owner != before:
-        await client.change_nickname(after, new_nick)
+async def on_member_join(member):
+    new_char = Character(member)
+    toons[member] = new_char
+    await client.change_nickname(member, new_char.level_tag + member.name)
+
+# TODO: UNTESTED
+@client.event
+async def on_member_remove(member):
+    temp_toon = toons[member]
+    del toons[member]
+    del temp_toon
+    await client.say("Another one bites the dust")
 
 
+# TODO: still raises an error when passed an empty string
 @client.command(pass_context=True,
                 name='changenickname',
                 aliases=['changenick', 'change_nick', 'change_nickname'])
-async def change_nickname(context, after):
+async def change_nickname(context, nick):
     author = context.message.author
     owner = context.message.server.owner
-    if author == client.user or author == context.message.server.owner:
+    if author == client.user:
         return
-    elif after.startswith("Lvl 1"):
-        client.say(author.mention + ", This nickname is invalid.")
+    elif nick.startswith("Lvl 1"):
+        await client.say(author.mention + ", This nickname is invalid.")
     elif author == owner:
-        client.say(author.mention + ", a bot does not have permission to " +
-                   "change a server owner's nickname.")
+        await client.say(author.mention + ", a bot does not have permission " +
+                         "to change a server owner's nickname.")
+    else:
+        await client.change_nickname(context.message.author, nick)
+        await client.say(author.mention + "your nickname is now: " + nick)
 
 
-
+# TODO: Add character functionality
 @client.command(pass_context=True,
                 name="russianroulette",
                 aliases=["rr", "russian_roulette", "russian"])
 async def russian_roulette(context):
     possible_responses = [
         " was just murdered to death...",
-        " has just been murdered! Reports claim that the victim knew the assailant",
+        " has just been murdered! Reports claim that the victim knew the "
+        + "assailant",
         " was their own worst enemy",
         " flew too close to the sun"
     ]
-    if context.message.author == client.user:
+    author = context.message.author
+    if author == client.user:
         return;
-    shot = random.randint(1, 6)
-    if shot == 6:
-        await client.say(context.message.author.mention +
-                         random.choice(possible_responses))
+    if random.randint(1, 6) == 6:
+        await client.say(author.mention + random.choice(possible_responses))
     else:
-        await client.say(context.message.author.mention + " gained 3 levels.")
+        await client.say(author.mention + " gained 3 levels.")
 
 
 client.run(TOKEN)
