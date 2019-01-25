32dda9278686a99847f2e8f109a410f778b3830a
Ben Wade <bmwade8@gmail.com>
2019-01-24 21:36:32 -0800

    pewdiepie fixed

--- pewdiepie.py
+++ pewdiepie.py
@@ -2,9 +2,8 @@ import requests
 import bs4
 
 
-# TODO: This doesn't even work, Marcy
 def get_pewdiepie_subs():
-    url = requests.get("https://socialblade.com/youtube/user/pewdiepie%22")
+    url = requests.get("https://socialblade.com/youtube/user/pewdiepie")
     soup = bs4.BeautifulSoup(url.text, "html.parser")
     sub_count = soup.findAll('span', {"id": "youtube-stats-header-subs"})
     return sub_count[0].get_text()
